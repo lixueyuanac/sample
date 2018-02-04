db112c5bba48d3ca7c7af315af4182e44c7bf298
xueyuan <654530249@qq.com>
2018-02-04 15:41:20 +0800

    修改

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('/signup','UsersController@create')->name('signup');
+Route::Resource('/users','UsersController');
 
