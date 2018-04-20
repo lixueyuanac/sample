50e741790b297f6676626f0b128b66dfaeecc3fe
vagrant <vagrant@homestead>
2018-04-20 02:52:09 +0000

    Update database configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,5 @@ class User extends Authenticatable
     public function gravatar($size='100'){
         $hash=md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
-
     }
 }
